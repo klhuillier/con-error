0bb111ea490b3cd64e59483152991359e7772614
Kevin L'Huillier <klhuillier@gmail.com>
2017-08-06 03:54:43 -0400

    removing more lambdas where declaration capturing of 'this' is inappropriate

--- src/formats.js
+++ src/formats.js
@@ -1,41 +1,51 @@
 const stackPreprocess = stack => stack;
 const colorize = stack => stack;
-const stringify = stack => JSON.stringify(stack, undefined, 0);
+const stringify = (stack, level) => JSON.stringify(stack, undefined, level);
 
 function Formats(error, config) {
   this.error = error;
   this.config = config;
 }
 
-Formats.prototype.fullStacks = () => new Formats(this.error, {
-  colorize: this.config.colorize,
-  fullStacks: true,
-  indent: this.config.indent,
-  jsonContexts: this.config.jsonContexts,
-});
+Formats.prototype.fullStacks = function() {
+  return new Formats(this.error, {
+    colorize: this.config.colorize,
+    fullStacks: true,
+    indent: this.config.indent,
+    jsonContexts: this.config.jsonContexts,
+  });
+};
 
-Formats.prototype.indent = level => new Formats(this.error, {
-  colorize: this.config.colorize,
-  fullStacks: this.config.fullStacks,
-  indent: level,
-  jsonContexts: this.config.jsonContexts,
-});
+Formats.prototype.indent = function(level) {
+  return new Formats(this.error, {
+    colorize: this.config.colorize,
+    fullStacks: this.config.fullStacks,
+    indent: level,
+    jsonContexts: this.config.jsonContexts,
+  });
+};
 
-Formats.prototype.jsonContexts = () => new Formats(this.error, {
-  colorize: this.config.colorize,
-  fullStacks: this.config.fullStacks,
-  indent: this.config.indent,
-  jsonContexts: true,
-});
+Formats.prototype.jsonContexts = function() {
+  return new Formats(this.error, {
+    colorize: this.config.colorize,
+    fullStacks: this.config.fullStacks,
+    indent: this.config.indent,
+    jsonContexts: true,
+  });
+};
 
-Formats.prototype.noColor = () => new Formats(this.error, {
-  colorize: false,
-  fullStacks: this.config.fullStacks,
-  indent: this.config.indent,
-  jsonContexts: this.config.jsonContexts,
-});
+Formats.prototype.noColor = function() {
+  return new Formats(this.error, {
+    colorize: false,
+    fullStacks: this.config.fullStacks,
+    indent: this.config.indent,
+    jsonContexts: this.config.jsonContexts,
+  });
+};
 
-Formats.prototype.json = () => stringify(this.error.stack(), this.config.indent);
+Formats.prototype.json = function() {
+  return stringify(this.error.stack(), this.config.indent);
+};
 
 function newFormat(error) {
   return new Formats(error, {
