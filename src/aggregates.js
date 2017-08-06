8e984433b8131709226948b125b63dfd6c96209c
Kevin L'Huillier <klhuillier@gmail.com>
2017-08-06 03:45:43 -0400

    aggregates seem to work

--- src/aggregates.js
+++ src/aggregates.js
@@ -2,33 +2,52 @@ function Aggregates(conError) {
   this.conError = conError;
 }
 
+// Input: causes: [A <- [X, Y <- 0], [B <- 1]]
+// 0 returns: [[0]]
+// Y returns: [[Y, 0]]
+// X returns: [[X]]
+// A returns: [[A, X], [A, Y, 0]]
+// 1 returns: [[1]]
+// B returns: [[B, 1]]
+// root returns: [[A, X], [A, Y, 0], [B, 1]]
+
 const buildChains = ce => {
-  const causes = ce.causes();
-  const newChildren = [];
-  causes.forEach(cause => {
-    if (typeof cause.causes === 'function') {
-      buildChains(cause).forEach(child => newChildren.push(child));
-    } else {
-      newChildren.push(cause);
-    }
-  });
-  return newChildren;
+  // If this isn't a ConError or it is a leaf ConError, return a set
+  // containing just this error
+  if (typeof ce !== 'object' || !Array.isArray(ce.causes) || ce.causes.length === 0) {
+    return [[ce]];
+  }
+
+  const result = [];
+
+  ce.causes.map(cause => buildChains(cause))
+    .forEach(cause => {
+      cause.forEach(chain => {
+        result.push([ce, ...chain]);
+      });
+    });
+
+  return result;
 };
 
 const flatten = ce => {
   const allErrors = [ce];
-  const stack = ce.causes();
+  const stack = Array.from(ce.causes);
   while (stack.length > 0) {
     const cause = stack.shift();
     allErrors.push(cause);
-    if (typeof cause.causes === 'function') {
-      cause.causes().forEach(child => stack.push(child));
+    if (Array.isArray(cause.causes)) {
+      cause.causes.forEach(child => stack.push(child));
     }
   }
   return allErrors;
 };
 
-Aggregates.prototype.chains = () => buildChains(this.conError);
-Aggregates.prototype.flatten = () => flatten(this.conError);
+Aggregates.prototype.chains = function() {
+  return buildChains(this.conError);
+};
+Aggregates.prototype.flatten = function() {
+  return flatten(this.conError);
+};
 
 module.exports = Aggregates;
