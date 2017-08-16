66a68cecd5bd73130b8ff8c45e5c2814f12f81ac
Kevin L'Huillier <klhuillier@gmail.com>
2017-08-16 03:45:46 -0400

    drop old src

--- src-old/dropped-top-stack-frame.js
+++
@@ -1,7 +0,0 @@
-function droppedTopStackFrameFactory(errorStackParser) {
-  return {
-    parse: err => errorStackParser.parse(err).slice(1)
-  };
-}
-
-module.exports = droppedTopStackFrameFactory;
